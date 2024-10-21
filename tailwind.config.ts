6fd560e7c8776141c1b7a7b3ad217a68a563b4a5
naosuke884 <884naoki.general@gmail.com>
2024-10-21 05:43:37 +0000

    settings vite, remix, typescript, tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    darkMode: ['class'],
+    content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
+    plugins: [require("tailwindcss-animate")],
+    theme: {
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {}
+    	}
+    }
+} satisfies Config
+
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import path from "path"
+
+export default defineConfig({
+  plugins: [
+    remix(),
+  ],
+  resolve: {
+    alias: {
+      "@/": path.join(__dirname, "app/"),
+    },
+  },
+});
