97187f439effc7040017639449e7ec3713c46fbd
naosuke884 <884naoki.general@gmail.com>
2024-12-11 07:56:44 +0000

    feat: contact page

---
+++ app/routes/contact_.thanks/route.tsx
@@ -0,0 +1,8 @@
+export default function ContactThanks() {
+  return (
+    <div className="flex flex-col items-center justify-start h-screen mt-10">
+      <h1 className="text-2xl font-bold">Thank you for your submission!</h1>
+      <p className="text-lg">We will get back to you as soon as possible.</p>
+    </div>
+  );
+}
