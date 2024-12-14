531cdb0df66cf8a418081b4c455f29fd9d4f0797
naosuke884 <884naoki.general@gmail.com>
2024-12-14 22:56:23 +0000

    style: mobile ui

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,21 +5,24 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 import MobileNav from "./HeaderMobileNav";
 import { navItems } from "./headerNavItems";
-import { cn } from "@/lib/utils";
 
 export default function App() {
+  const location = useLocation();
+  const rootPathName = location.pathname.split("/")[1];
+  const triggerText =
+    rootPathName.charAt(0).toUpperCase() + rootPathName.slice(1);
   return (
-    <header className="flex h-24 p-5 ring">
+    <header className="flex h-24 p-5 text-xl font-semibold ring">
       <div className="mr-auto flex items-center justify-center">
         <Link to="/about" className="text-2xl font-extrabold tracking-tight">
           {"Hayashi Naoki"}
         </Link>
       </div>
-      <MobileNav className="sm:hidden" />
-      <NavigationMenu className="hidden sm:block">
+      <MobileNav className="sm:hidden" triggerText={triggerText} />
+      <NavigationMenu className="hidden sm:flex">
         <NavigationMenuList>
           {navItems.map((item) => (
             <NavigationMenuItem key={item.key}>
@@ -27,9 +30,7 @@ export default function App() {
                 className={navigationMenuTriggerStyle()}
                 asChild
               >
-                <Link to={item.url} className="text-xl font-semibold">
-                  {item.text}
-                </Link>
+                <Link to={item.url}>{item.text}</Link>
               </NavigationMenuLink>
             </NavigationMenuItem>
           ))}
